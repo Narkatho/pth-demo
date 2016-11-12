8ce248f60f6b8438d25355d5931bd28eea865b63
Narkatho <narkatho@gmail.com>
2016-11-12 12:14:43 -0300

    Avances en las opciones

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -6,10 +6,46 @@ $('#container').resizable({
   }
 });
 
-$('.column.left').resizable({ // ver el max width más adelante (seguro que será según el contenido de RC)
+$('.column.left').resizable({
   handles: 'e',
   minWidth: 199,
   resize: function (event, ui) {
-    $('.column.right').width($(document).width() - (ui.size.width + 1));
+    $('.column.right').width(window.innerWidth - (ui.size.width + 1));
   }
+});
+
+var options = {};
+
+$('.column.left li:not(.disabled)').click(function () {
+  var optionID = $(this).data('optionid');
+
+  if (options.hasOwnProperty(optionID)) {
+    if (options[optionID].isActive) return false;
+  } else {
+    options[optionID] = {
+      $el: $('<ul>ALGO ' + optionID + '</ul>'),
+      status: 0
+    };
+
+    options[optionID].$el.appendTo('.column.right > div');
+  }
+
+  if (options.activeOptionID) {
+    $('.column.left li[data-optionID=' + options.activeOptionID + ']').css({
+      backgroundColor: 'initial',
+      color: 'initial'
+    });
+
+    options[options.activeOptionID].isActive = false;
+    options[options.activeOptionID].$el.hide();
+  }
+
+  $(this).css({
+    backgroundColor: 'rgb(56, 121, 217)',
+    color: 'white'
+  });
+
+  options.activeOptionID = optionID;
+  options[optionID].isActive = true;
+  options[optionID].$el.show();
 });
