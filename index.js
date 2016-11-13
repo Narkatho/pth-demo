bb9e43b934d0f56aa9b800d48269771626910243
Narkatho <narkatho@gmail.com>
2016-11-13 00:41:47 -0300

    puerto 80

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
   res.render(__dirname + '/app/views/index', {});
 });
 
-app.listen(8000, function () {
-  console.log('server listening on port 8000');
+app.listen(80, function () {
+  console.log('server listening on port 80');
 });
