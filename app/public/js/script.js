56bd1c2bdaccb668e2d239553d8254355fcb0f2c
Narkatho <narkatho@gmail.com>
2016-11-14 11:17:25 -0300

    login

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -121,16 +121,211 @@ var options = {
 
         break;
       case 2: // Leak de info privada por JSONP
+        switch (this[2].status) {
+          case 1:
+            this[2].status = 2;
+
+            this[2].$el.append(
+              '<li class="pth-message">' +
+                'El endpoint <span>GET http://examplecdn.com/js/script.js?callback=__smth__</spa' +
+                'n> parece hacer uso del patrón JSONP. Si la información que transporta es priva' +
+                'da entonces corres el riesgo de que dicha información pueda ser accedida por te' +
+                'rceros sin autorización.' +
+                '<br><br>' +
+                '¿La información que viaja en el response JSONP es privada?' +
+                '<br><br>' +
+                '<button value="1">Sí</button><button value="2">No</button>' +
+              '</li>');
+
+            break;
+          case 2:
+            this[2].status = 3;
+
+            if (algo == 1) {
+              this[2].$el.append(
+                '<li class="pth-message">' +
+                  'El patrón JSONP no debe ser usado para procesar información privada pues pued' +
+                  'e ser llamado desde dominios arbitrarios de terceros. En el caso particular d' +
+                  'e esta llamada, cualquier dominio puede acceder a la información privada cont' +
+                  'enida en esta llamada simplemente agregando esta porción de HTML en su código' +
+                  ' fuente' +
+                  '<br><br>' +
+                  '<span class="code">&lt;script type=&rdquo;text/javascript&rdquo;' +
+                  '<br>' +
+                  'src=&rdquo;http://examplecdn.com/js/script.js?callback=__smth__&rdquo;&gt;&lt' +
+                  ';/script&gt;</span>' +
+                  '<br><br>' +
+                  'Para resolver este problema se recomienda dejar de hacer uso completamente de' +
+                  'l patrón JSONP y en su lugar retornar la información como un simple objeto JS' +
+                  'ON a través de una llamada AJAX. Esto debiese ser bastante sencillo si la inf' +
+                  'ormación transportada en el JSONP no debe ser compartida con un dominio de te' +
+                  'rceros. Por otra parte, si resulta necesario compartir la información con otr' +
+                  'os dominios, igualmente se recomienda retornar un simple objeto JSON a través' +
+                  ' de una llamada AJAX, pero utilizando CORS para compartir la información con ' +
+                  'el dominio específico que debe tener acceso a ella.' +
+                '<li>');
+            } else {
+              this[2].$el.append(
+                '<li class="pth-message">' +
+                  '¿Estás seguro que la información que viaja en JSONP es privada y por lo tanto' +
+                  ' este patrón JSONP no supone un filtrado de información hacia terceros?' +
+                  '<br><br>' +
+                  'Información privada es todo tipo de información que un tercero, que no es pro' +
+                  'pietario de dicha asignación, no debiese poder ver tal como PII (Personally I' +
+                  'dentifiable Information)' +
+                  '<br><br>' +
+                  '<button value="1">Sí, y marcar como no vulnerable</button><button value="2">N' +
+                  'o, la información sí es privada</button>' +
+                '</li>');
+            }
+
+            break;
+          case 3:
+            if (algo == 1) {
+              alert('Marcado como no vulnerable');
+            } else {
+              alert('Marcado como vulnerable');
+            }
+
+            break;
+        }
+
         break;
       case 3: // Leak de info privada por JS
+        switch (this[3].status) {
+          case 1:
+            this[3].status = 2;
+
+            this[3].$el.append( // hay que ver esto, no entiendo, preguntarle a mi tio
+              '<li class="pth-message">' +
+                'El javascript <span>GET http://example.com/static/nonlib.js</span> es llamado d' +
+                'esde un recurso privado. Resulta importante comprobar si esta llamada JS no tra' +
+                'nsporta información privada. Por favor ejecuta el siguiente request que ayudará' +
+                ' a comprobar si existe leak de información privada en este JS' +
+                '<br><br>' +
+                '<button value="1">REQUEST GET http://example.com/static/nonlib.js</button> (sin' +
+                ' auth)' +
+                '<br><br>' +
+                '<button value="1">REQUEST GET http://example.com/static/nonlib.js</button> (con' +
+                ' auth)' +
+              '</li>');
+
+            break;
+          case 2:
+            this[3].status = 3;
+            break;
+          case 3:
+            break;
+        }
+
         break;
       case 4: // Detectar inyección CSV
+        switch (this[4].status) {
+          case 1:
+            this[4].status = 2;
+
+            this[4].$el.append(
+              '<li class="pth-message">' +
+                'We just saw a CSV file being downloaded. Does this file contain fields whose va' +
+                'lues are direct input from the user of your webapp?' +
+                '<br><br>' +
+                '<button value="1">Yes</button><button value="2">No</button>' +
+              '</li>');
+
+            break;
+          case 2:
+            this[4].status = 3;
+
+            if (algo == 1) {
+              this[4].path = 1;
+
+              this[4].$el.append(
+                '<li class="pth-message">' +
+                  'OK. There’s a chance this endpoint can be used to build malicious CSV files. ' +
+                  'To test we need your help.' +
+                  '<br><br>' +
+                  'Please go to the formulary whose input is later shown as a field in this CSV ' +
+                  'field and enter this value' +
+                  '<br><br>' +
+                  '<span class="code">=1+1</span>' +
+                  '<br><br>' +
+                  'And save it. Then, go back to this endpoint and check that the CSV file shows' +
+                  ' this value too as a field.' +
+                  '<br><br>' +
+                  '<button value="1">Yes, it’s there!</button><button value="2">No, it’s not the' +
+                  're</button>' +
+                '</li>');
+            } else {
+              this[4].path = 2;
+
+              this[4].$el.append(
+                '<li class="pth-message">' +
+                  'You sure?' +
+                  '<br><br>' +
+                  '<button value="1">Yes, I’m sure</button><button value="2">No, I think some of' +
+                  ' these fields actually come directly from user input</button>' +
+                '</li>');
+            }
+
+            break;
+          case 3:
+            if (algo == 1) {
+              if (this[4].path === 1) {
+                this[4].$el.append(
+                  '<li class="pth-message">' +
+                    'Awesome! (or not? it depends on who’s looking :P) This little nasty bug can' +
+                    ' be used to inject malicious Excel commands in this CSV file and however ge' +
+                    'ts to download it (maybe the boss herself??) will have a hard time on her c' +
+                    'omputer.' +
+                    '<br><br>' +
+                    'So we need to fix this. The way to do it is simple: filter this CSV fields ' +
+                    'to escape initial “=” characters. That way they won’t execute as Excel form' +
+                    'ulas. Therefore if a field is' +
+                    '<br><br>' +
+                    '<span class="code">=1+1</span>' +
+                    '<br><br>' +
+                    'then it should be sent as' +
+                    '<br><br>' +
+                    '<span class="code">\\=1+1</span>' +
+                    '<br><br>' +
+                    'Cool?' +
+                  '</li>');
+              } else {
+                alert('Aquí termina (A1 P2)');
+              }
+            } else {
+              if (this[4].path === 1) {
+                alert('Aquí termina (A2 P1)');
+              } else {
+                alert('Aquí termina (A2 P2)');
+              }
+            }
+            break;
+        }
+
         break;
     }
   }
 };
 
-$('.column.left li:not(.disabled)').click(function () {
+$('#login button').click(function () {
+  $('#login').hide();
+  $('#columns').show();
+
+  setTimeout(function () {
+    $('.column.left ul').prepend(
+      '<li data-optionID="3">Leak de info privada por JS</li>' +
+      '<li data-optionID="4">Detectar inyección CSV</li>');
+  }, 1500);
+
+  setTimeout(function () {
+    $('.column.left ul').prepend(
+      '<li data-optionID="1">XXE</li>' +
+      '<li data-optionID="2">Leak de info privada por JSONP</li>');
+  }, 3000);
+});
+
+$(document).on('click', '.column.left li:not(.disabled)', function () {
   var optionID = $(this).data('optionid');
 
   if (options.activeOptionID) {
@@ -168,5 +363,7 @@ $('.column.left li:not(.disabled)').click(function () {
 });
 
 $(document).on('click', '.pth-message button', function () {
+  $(this).parent().find('button').prop('disabled', true);
+  $(this).addClass('selected');
   options.algo($(this).val());
 });
